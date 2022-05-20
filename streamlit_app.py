7fb3767edf80942de836a51186db7392431780c7
Maria-Freitas25 <101955874+Maria-Freitas25@users.noreply.github.com>
2022-05-20 19:34:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
 import streamlit as st
 
-def main ():
-  st.title("Teste StreamLit")
-  st.subheader("How to run streamlit from colab")
-  menu = ["Home","About"]
-  choise = st.side.selectbox('Menu',menu)
-  if choice == 'Home':
-    st.subheader("Streamlit from Colab")
-#if name == 'main':
- #   main()
-def chamar():
-  main()
+st.title('Construindo Relatórios para Data Science com Streamlit')
