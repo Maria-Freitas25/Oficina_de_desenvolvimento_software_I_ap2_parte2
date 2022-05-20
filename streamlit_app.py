9329100e2d22efb836e2ba113df0f2f033db9042
Maria-Freitas25 <101955874+Maria-Freitas25@users.noreply.github.com>
2022-05-20 19:28:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
-st.beta_set_page_config(**PAGE_CONFIG)
+
 def main ():
   st.title("Teste StreamLit")
   st.subheader("How to run streamlit from colab")
