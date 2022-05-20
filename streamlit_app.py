d42f78873148eedcbb1b2f3a82c0551000b0dcff
Maria-Freitas25 <101955874+Maria-Freitas25@users.noreply.github.com>
2022-05-20 19:27:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,11 @@
-def main():
-	st.title("Awesome Streamlit for ML")
-	st.subheader("How to run streamlit from colab")
+st.title("Awesome Streamlit for ML")
+st.subheader("How to run streamlit from colab")
 
 
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox('Menu',menu)
-	if choice == 'Home':
-		st.subheader("Streamlit From Colab")	
-
-
+menu = ["Home","About"]
+choice = st.sidebar.selectbox('Menu',menu)
+if choice == 'Home':
+st.subheader("Streamlit From Colab")	
 
 if __name__ == '__main__':
 	main()
