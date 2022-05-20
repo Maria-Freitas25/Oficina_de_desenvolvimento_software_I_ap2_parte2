adcd3aa7454dae6ef6feb630f934fc25b9f28243
Maria-Freitas25 <101955874+Maria-Freitas25@users.noreply.github.com>
2022-05-20 19:36:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.title('MEU DEUS MEU SENHOR ME AJUDE POR FAVOR')
+st.markdown('---')
+st.markdown('# Sobre a Base de Dados')
+st.markdown('''O naufrágio do titanic aconteceu em 15 de abril de 1992 no atlântico norte colidindo com um iceberg.
+Quatro dias depois de sua viagem inaugural, o considerado ‘inafundável’ navio deixou 1502 mortos dos 2224 passageiros e tripulantes.
+Mesmo com um fator sorte envolvido no desastre parece que alguns grupos tiveram mais chances que outros de sobrevivência.
+Vamos trabalhar com A base de dados : 
+conjunto de treinamento (train.csv)
+__*Dicionário dos dados*__
+- PassengerId (Número de identificação dos passageiros)
+- survival (Sobrevivencia, 0 = não e 1 = sim)
+- pclass (Classe de ingresso, 1 = 1° 2 = 2° 3 = 3° )
+- Name (Nome do passageiro)
+- sex (Sexo do passageiro)
+- Age (Idade)
+- sibsp ( irmãos / cônjuges a bordo do Titanic)
+- parch (de pais / filhos a bordo do Titanic)
+- ticket (Número do bilhete)
+- fare (Tarifa de passageiro)
+- cabin (Número da cabine)
+- embarked (Porto de embarcação , C = Cherbourg, Q = Queenstown, S = Southampton)''')
+st.markdown('---')
